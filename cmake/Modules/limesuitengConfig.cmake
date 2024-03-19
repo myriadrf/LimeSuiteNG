bd7766dae6a17d93a73d142692d97aa25a760ea9
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2024-03-19 18:34:51 +0200

    Update limesuiteng cmake find package files

--- cmake/Modules/limesuitengConfig.cmake
+++ cmake/Modules/limesuitengConfig.cmake
@@ -1,16 +1,16 @@
-if(DEFINED INCLUDED_LIMESUITE_CONFIG_CMAKE)
+if(DEFINED INCLUDED_LIMESUITENG_CONFIG_CMAKE)
   return()
 endif()
-set(INCLUDED_LIMESUITE_CONFIG_CMAKE TRUE)
+set(INCLUDED_LIMESUITENG_CONFIG_CMAKE TRUE)
 
 ########################################################################
-# LimeSuiteConfig - cmake project configuration for client clibraries
+# LimeSuiteNGConfig - cmake project configuration for client clibraries
 #
 # The following will be set after find_package(LimeSuite):
-# LimeSuite_LIBRARIES    - development libraries
-# LimeSuite_INCLUDE_DIRS - development includes
+# limesuiteng_LIBRARIES    - development libraries
+# limesuiteng_INCLUDE_DIRS - development includes
 #
-# Or link with the import library target LimeSuite
+# Or link with the import library target limesuiteng
 ########################################################################
 
 ########################################################################
@@ -27,32 +27,32 @@ endif ()
 ## locate the library
 ########################################################################
 find_library(
-  LIMESUITE_LIBRARY LimeSuite
+  LIMESUITENG_LIBRARY limesuiteng
   PATHS ${LIMESUITE_ROOT}/lib${LIB_SUFFIX}
   PATH_SUFFIXES ${CMAKE_LIBRARY_ARCHITECTURE}
   NO_DEFAULT_PATH
   )
-if(NOT LIMESUITE_LIBRARY)
+if(NOT LIMESUITENG_LIBRARY)
   message(FATAL_ERROR "cannot find LimeSuite library in ${LIMESUITE_ROOT}/lib${LIB_SUFFIX}")
 endif()
-set(LimeSuite_LIBRARIES ${LIMESUITE_LIBRARY})
+set(limesuiteng_LIBRARIES ${LIMESUITENG_LIBRARY})
 
 ########################################################################
 ## locate the includes
 ########################################################################
 find_path(
-  LIMESUITE_INCLUDE_DIR limesuite/SDRDevice.h
+  LIMESUITENG_INCLUDE_DIR limesuite/SDRDevice.h
   PATHS ${LIMESUITE_ROOT}/include
   NO_DEFAULT_PATH
 )
-if(NOT LIMESUITE_INCLUDE_DIR)
+if(NOT LIMESUITENG_INCLUDE_DIR)
   message(FATAL_ERROR "cannot find LimeSuite includes in ${LIMESUITE_ROOT}/include")
 endif()
-set(LimeSuite_INCLUDE_DIRS ${LIMESUITE_INCLUDE_DIR})
+set(limesuiteng_INCLUDE_DIRS ${LIMESUITENG_INCLUDE_DIR})
 
 ########################################################################
 ## create import library target
 ########################################################################
 add_library(LimeSuite SHARED IMPORTED)
-set_property(TARGET LimeSuite PROPERTY IMPORTED_LOCATION ${LIMESUITE_LIBRARY})
-set_property(TARGET LimeSuite PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${LIMESUITE_INCLUDE_DIR})
+set_property(TARGET LimeSuite PROPERTY IMPORTED_LOCATION ${LIMESUITENG_LIBRARY})
+set_property(TARGET LimeSuite PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${LIMESUITENG_INCLUDE_DIR})
