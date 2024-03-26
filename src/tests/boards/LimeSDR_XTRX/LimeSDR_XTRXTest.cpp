418bcf1402435170c72841e14d0ac01b27320bdd
Dominykas <dominykas.petkevicius@matom.ai>
2024-03-26 13:38:23 +0200

    Add XTRX and X3 connection tests

---
+++ src/tests/boards/LimeSDR_XTRX/LimeSDR_XTRXTest.cpp
@@ -0,0 +1,36 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "LimeSDR_XTRX.h"
+#include "limesuite/DeviceRegistry.h"
+
+using namespace lime;
+using namespace std::literals::string_literals;
+
+class LimeSDR_XTRX_Fixture : public ::testing::Test
+{
+  protected:
+    void SetUp() override
+    {
+        auto devices = DeviceRegistry::enumerate(DeviceHandle{ deviceHandleHint });
+
+        if (devices.empty())
+        {
+            GTEST_SKIP() << "LimeSDR-XTRX not connected, skipping"s;
+        }
+
+        device = DeviceRegistry::makeDevice(DeviceHandle{ deviceHandleHint });
+
+        ASSERT_NE(device, nullptr);
+    }
+
+    void TearDown() override { DeviceRegistry::freeDevice(device); }
+
+    inline static const std::string deviceHandleHint{ "LimeXTRX0"s };
+
+    SDRDevice* device = nullptr;
+};
+
+TEST_F(LimeSDR_XTRX_Fixture, ConnectToDevice)
+{
+}
