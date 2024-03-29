d347f0f0f908a632330ab5e57022f55d76e3636d
Dominykas Petkevičius <dominykas.petkevicius@matom.ai>
2024-03-29 15:31:30 +0200

    Fix USB connections on Windows

--- src/CommonFunctions.h
+++ src/CommonFunctions.h
@@ -1,10 +1,21 @@
 #pragma once
 
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 #include "limesuiteng/commonTypes.h"
 
 namespace lime {
+
 const std::string strFormat(const char* format, ...);
 std::string ToString(TRXDir dir);
+
+template<typename T> std::string intToHex(T i)
+{
+    std::stringstream stream;
+    stream << std::setfill('0') << std::setw(sizeof(T) * 2) << std::hex << i;
+    return stream.str();
+}
+
 } // namespace lime
