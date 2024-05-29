9f51dafd7a553e4d4e6832ddf5c508ec787d1a6a
Dominykas <dominykas.petkevicius@matom.ai>
2024-05-29 11:18:42 +0300

    Update pllsweep to use the args library

--- src/utilities/pll_sweep.cpp
+++ src/utilities/pll_sweep.cpp
@@ -16,177 +16,22 @@
 #include <fstream>
 #include <chrono>
 #include <cmath>
+#include "args/args.hxx"
 
-using namespace std;
 using namespace lime;
-
-auto t1 = chrono::high_resolution_clock::now();
-auto t2 = chrono::high_resolution_clock::now();
-
-bool ConfigureCGEN(SDRDevice* device, double freqMHz);
-
-int printHelp(void);
-
-lime::LogLevel log_level = lime::LogLevel::Info;
-
-void log_func(const lime::LogLevel level, const std::string& message)
-{
-    if (level <= log_level)
-        std::cout << message << std::endl;
-}
-
-int main(int argc, char** argv)
-{
-    double beginFreq = 50e6;
-    double endFreq = 500e6;
-    double stepFreq = 1e6;
-    string configFilename = "";
-    bool init = false;
-    int deviceIndex = -1;
-
-    int c;
-    while (1)
-    {
-        static struct option long_options[] = { { "beginFreq", required_argument, 0, 'b' },
-            { "stepFreq", required_argument, 0, 's' },
-            { "endFreq", required_argument, 0, 'e' },
-            { "log", required_argument, 0, 'l' },
-            { "config", required_argument, 0, 'c' },
-            { "device", required_argument, 0, 'd' },
-            { "help", no_argument, 0, 'h' },
-            { 0, 0, 0, 0 } };
-        /* getopt_long stores the option index here. */
-        int option_index = 0;
-        c = getopt_long(argc, argv, "b:s:e:l:c::d:h", long_options, &option_index);
-
-        if (c == -1) //no parameters given
-            break;
-        switch (c)
-        {
-        case 'b': {
-            stringstream ss;
-            ss << optarg;
-            ss >> beginFreq;
-            break;
-        }
-        case 's': {
-            stringstream ss;
-            ss << optarg;
-            ss >> stepFreq;
-            break;
-        }
-        case 'e': {
-            stringstream ss;
-            ss << optarg;
-            ss >> endFreq;
-            break;
-        }
-        case 'l': {
-            stringstream ss;
-            ss << optarg;
-            int tempInt = 0;
-            ss >> tempInt;
-            log_level = static_cast<LogLevel>(tempInt);
-            break;
-        }
-        case 'c': {
-            if (optarg != NULL)
-            {
-                stringstream ss;
-                ss << optarg;
-                ss >> configFilename;
-            }
-            else
-                init = true;
-            break;
-        }
-        case 'd': {
-            stringstream ss;
-            ss << optarg;
-            ss >> deviceIndex;
-            break;
-        }
-        case 'h':
-            printHelp();
-            return 0;
-        case '?':
-            /* getopt_long already printed an error message. */
-            break;
-        default:
-            std::cout << "Invalid option" << std::endl;
-            abort();
-        }
-    }
-
-    cout << "beginFreq = " << beginFreq / 1e6 << " MHz" << endl;
-    cout << "stepFreq  = " << stepFreq / 1e6 << " MHz" << endl;
-    cout << "endFreq   = " << endFreq / 1e6 << " MHz" << endl;
-
-    SDRDevice* device;
-    std::vector<DeviceHandle> handles = DeviceRegistry::enumerate();
-    if (handles.size() == 0)
-    {
-        cout << "No devices found" << endl;
-        return -1;
-    }
-    if (handles.size() == 1) //open the only available device
-        device = DeviceRegistry::makeDevice(handles.at(0));
-    else //display device selection
-    {
-        if (deviceIndex < 0)
-        {
-            cout << "Device list:" << endl;
-            for (size_t i = 0; i < handles.size(); i++)
-                cout << setw(2) << i << ". " << handles[i].name << endl;
-            cout << "Select device index (0-" << handles.size() - 1 << "): ";
-            int selection = 0;
-            cin >> selection;
-            selection = selection % handles.size();
-            device = DeviceRegistry::makeDevice(handles.at(selection));
-        }
-        else
-            device = DeviceRegistry::makeDevice(handles.at(deviceIndex));
-    }
-    if (device == nullptr)
-    {
-        cout << "Failed to connected to device" << endl;
-        return -1;
-    }
-    auto& info = device->GetDescriptor();
-    cout << "\nConnected to: " << info.name << " FW: " << info.firmwareVersion << " HW: " << info.hardwareVersion << endl;
-
-    if (configFilename.length() > 0)
-    {
-        if (device->LoadConfig(0, configFilename) != OpStatus::Success)
-        {
-            return -1;
-        }
-    }
-    else if (init)
-    {
-        device->Init();
-    }
-
-    lime::registerLogHandler(log_func);
-    int errors = 0;
-    for (double freq = beginFreq; freq < endFreq; freq += stepFreq)
-        if (ConfigureCGEN(device, freq) == false)
-            errors++;
-    cout << "Errors: " << errors << endl;
-    delete device;
-    return 0;
-}
+using namespace std::literals::string_literals;
+using namespace std::literals::string_view_literals;
 
 bool ConfigureCGEN(SDRDevice* device, double freqMHz)
 {
-    std::cout << "Set CGEN " << freqMHz / 1e6 << " MHz: ";
+    std::cout << "Set CGEN "sv << freqMHz / 1e6 << " MHz: "sv;
     LMS7002M* lms = static_cast<LMS7002M*>(device->GetInternalChip(0));
     lms->Modify_SPI_Reg_bits(LMS7002MCSR::MAC, 1, true);
     int interp = lms->Get_SPI_Reg_bits(LMS7002MCSR::HBI_OVR_TXTSP);
     int decim = lms->Get_SPI_Reg_bits(LMS7002MCSR::HBD_OVR_RXTSP);
     if (lms->SetInterfaceFrequency(freqMHz, interp, decim) != OpStatus::Success)
     {
-        std::cout << "LMS VCO Fail" << endl;
+        std::cout << "LMS VCO Fail"sv << std::endl;
         return false;
     }
 
@@ -203,28 +48,133 @@ bool ConfigureCGEN(SDRDevice* device, double freqMHz)
         OpStatus status = fpga->SetInterfaceFreq(fpgaTxPLL, fpgaRxPLL, chipInd);
         if (status != OpStatus::Success)
         {
-            std::cout << "FPGA Fail" << endl;
+            std::cout << "FPGA Fail"sv << std::endl;
             return false;
         }
     }
-    std::cout << "OK" << endl;
+    std::cout << "OK"sv << std::endl;
     return true;
 }
 
-/***********************************************************************
- * print help
- **********************************************************************/
-int printHelp(void)
+lime::LogLevel log_level = lime::LogLevel::Info;
+static LogLevel strToLogLevel(const std::string_view str)
 {
-    std::cout << "Usage pll_sweep [options]" << std::endl;
-    std::cout << "available options:" << std::endl;
-    std::cout << "    --help \t\t Print this help message" << std::endl;
-    std::cout << "    --beginFreq \t sweep start frequency" << std::endl;
-    std::cout << "    --endFreq \t\t sweep end frequency" << std::endl;
-    std::cout << "    --stepFreq \t\t sweep frequency step" << std::endl;
-    std::cout << "    --log \t\t log level" << std::endl;
-    std::cout << "    --config \t\t load config" << std::endl;
-    std::cout << "    --device \t\t device index" << std::endl;
-    std::cout << std::endl;
-    return EXIT_SUCCESS;
+    if ("debug"sv == str)
+        return LogLevel::Debug;
+    else if ("verbose"sv == str)
+        return LogLevel::Verbose;
+    else if ("info"sv == str)
+        return LogLevel::Info;
+    else if ("warning"sv == str)
+        return LogLevel::Warning;
+    else if ("error"sv == str)
+        return LogLevel::Error;
+    else if ("critical"sv == str)
+        return LogLevel::Critical;
+    return LogLevel::Error;
+}
+
+void log_func(const lime::LogLevel level, const std::string& message)
+{
+    if (level <= log_level)
+        std::cout << message << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    // clang-format off
+    args::ArgumentParser            parser("pll_sweep - sweeps through the PLL range"s, ""s);
+    args::HelpFlag                  help(parser, "help"s, "This help"s, {'h', "help"s});
+
+    args::ValueFlag<double>         startFreqFlag(parser, "beginFreq"s, "Sweep start frequency"s, {'b', "beginFreq"s}, 50e6);
+    args::ValueFlag<double>         endFreqFlag(parser, "endFreq"s, "Sweep end frequency"s, {'e', "endFreq"s}, 500e6);
+    args::ValueFlag<double>         stepFreqFlag(parser, "stepFreq"s, "Sweep frequency step"s, {'s', "stepFreq"s}, 1e6);
+
+    args::ValueFlag<int>            deviceFlag(parser, "device"s, "Specifies which device to use"s, {'d', "device"s}, -1);
+    args::ValueFlag<std::string>    logFlag(parser, "level"s, "Log verbosity levels: error, warning, info, verbose, debug"s, {'l', "log"s}, "info"s);
+
+    args::Group                     group(parser, "This group is all exclusive:"s, args::Group::Validators::AtMostOne);
+    args::ValueFlag<std::string>    iniFlag(group, "config"s, "Path to LMS7002M .ini configuration file to use as a base"s, {'c', "config"s}, ""s);
+    args::Flag                      initFlag(group, "init"s, "Just initialize the device"s, {'i', "init"s}, false);
+    // clang-format on
+
+    try
+    {
+        parser.ParseCLI(argc, argv);
+    } catch (args::Help&)
+    {
+        std::cout << parser << std::endl;
+        return EXIT_SUCCESS;
+    } catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    log_level = strToLogLevel(args::get(logFlag));
+
+    const auto beginFreq = args::get(startFreqFlag);
+    const auto stepFreq = args::get(stepFreqFlag);
+    const auto endFreq = args::get(endFreqFlag);
+
+    std::cout << "beginFreq = "sv << beginFreq / 1e6 << " MHz"sv << std::endl;
+    std::cout << "stepFreq  = "sv << stepFreq / 1e6 << " MHz"sv << std::endl;
+    std::cout << "endFreq   = "sv << endFreq / 1e6 << " MHz"sv << std::endl;
+
+    SDRDevice* device;
+    const std::vector<DeviceHandle> handles = DeviceRegistry::enumerate();
+    if (handles.size() == 0)
+    {
+        std::cout << "No devices found"sv << std::endl;
+        return -1;
+    }
+    if (handles.size() == 1) //open the only available device
+        device = DeviceRegistry::makeDevice(handles.at(0));
+    else //display device selection
+    {
+        const auto deviceIndex = args::get(deviceFlag);
+        if (deviceIndex < 0)
+        {
+            std::cout << "Device list:"sv << std::endl;
+            for (size_t i = 0; i < handles.size(); i++)
+                std::cout << std::setw(2) << i << ". "sv << handles[i].name << std::endl;
+            std::cout << "Select device index (0-"sv << handles.size() - 1 << "): "sv;
+            int selection = 0;
+            std::cin >> selection;
+            selection = selection % handles.size();
+            device = DeviceRegistry::makeDevice(handles.at(selection));
+        }
+        else
+            device = DeviceRegistry::makeDevice(handles.at(deviceIndex));
+    }
+    if (device == nullptr)
+    {
+        std::cout << "Failed to connected to device"sv << std::endl;
+        return -1;
+    }
+    auto& info = device->GetDescriptor();
+    std::cout << "\nConnected to: "sv << info.name << " FW: "sv << info.firmwareVersion << " HW: " << info.hardwareVersion
+              << std::endl;
+
+    const auto configFilename = args::get(iniFlag);
+    if (configFilename.length() > 0)
+    {
+        if (device->LoadConfig(0, configFilename) != OpStatus::Success)
+        {
+            return -1;
+        }
+    }
+    else if (args::get(initFlag))
+    {
+        device->Init();
+    }
+
+    lime::registerLogHandler(log_func);
+    int errors = 0;
+    for (double freq = beginFreq; freq < endFreq; freq += stepFreq)
+        if (ConfigureCGEN(device, freq) == false)
+            errors++;
+    std::cout << "Errors: "sv << errors << std::endl;
+    DeviceRegistry::freeDevice(device);
+    return 0;
 }
