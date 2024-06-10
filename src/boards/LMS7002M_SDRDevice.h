730fe73069439a7866a5be8bc8d4199000103263
Dominykas <dominykas.petkevicius@matom.ai>
2024-06-10 10:22:37 +0300

    Run formatters

--- src/boards/LMS7002M_SDRDevice.h
+++ src/boards/LMS7002M_SDRDevice.h
@@ -126,6 +126,7 @@ class LIME_API LMS7002M_SDRDevice : public SDRDevice
     virtual OpStatus WriteFPGARegister(uint32_t address, uint32_t value);
 
     FPGA* GetFPGA();
+
   protected:
     static OpStatus UpdateFPGAInterfaceFrequency(LMS7002M& soc, FPGA& fpga, uint8_t chipIndex);
     void SetGainInformationInDescriptor(RFSOCDescriptor& descriptor);
