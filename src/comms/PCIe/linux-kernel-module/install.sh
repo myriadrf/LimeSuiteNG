1a3f60e217e1da654f72dc3373844d76be4e410b
Dominykas <dominykas.petkevicius@matom.ai>
2024-06-21 16:14:41 +0300

    Add fallback to not use `dkms` if not available

--- src/comms/PCIe/linux-kernel-module/install.sh
+++ src/comms/PCIe/linux-kernel-module/install.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
-KERNEL_DRIVER_VERSION="$(cat version)"
+
+function dkms_install {
+    SOURCE_FILES="Makefile limepcie.c limepcie.h boards.h bsp/"
+    KERNEL_DRIVER_VERSION="$(cat version)"
+
+    mkdir -p "/usr/src/limepcie-$KERNEL_DRIVER_VERSION"
+    cp -r $SOURCE_FILES "/usr/src/limepcie-$KERNEL_DRIVER_VERSION"
+    sed -e "s/@DRIVER_VERSION@/$KERNEL_DRIVER_VERSION/" dkms.conf.in >"/usr/src/limepcie-$KERNEL_DRIVER_VERSION/dkms.conf"
+
+    # If exists just reinstall
+    MODULE_AND_VERSION=$(dkms status | grep "limepcie" | cut -d ',' -f 1)
+    if [ "$MODULE_AND_VERSION" != "" ]; then
+        dkms remove -m "$MODULE_AND_VERSION"
+    else
+        dkms add -m "limepcie/$KERNEL_DRIVER_VERSION"
+    fi
+
+    dkms install -m "limepcie/$KERNEL_DRIVER_VERSION"
+}
+
+function legacy_install {
+    LOG_FILE=/var/tmp/$MODULE_NAME.log
+
+    if ! lsmod | grep -Eq "^$MODULE_NAME"; then
+        depmod
+        modprobe -v "$MODULE_NAME" >>"$LOG_FILE"
+
+        if lsmod | grep -Eq "^$MODULE_NAME"; then
+            echo "Loaded kernel module: \"modprobe $MODULE_NAME\""
+            echo "SUCCESS: $MODULE_NAME module loaded" >>"$LOG_FILE"
+        else
+            echo "Failed to load kernel module: \"modprobe $MODULE_NAME\""
+            echo "FAILURE: $MODULE_NAME install failed" >>"$LOG_FILE"
+            exit 1
+        fi
+    else
+        echo "$MODULE_NAME module was loaded successfully" >>"$LOG_FILE"
+    fi
+}
 
 # Check root access
 if [ "$EUID" -ne 0 ]; then
@@ -11,11 +49,34 @@ if [ -f uninstall-legacy-module.sh ]; then
     ./uninstall-legacy-module.sh
 fi
 
-SOURCE_FILES="limepcie.c limepcie.h boards.h bsp/"
-cp -r $SOURCE_FILES "/usr/src/limepcie-$KERNEL_DRIVER_VERSION"
-sed -e "s/@DRIVER_VERSION@/$KERNEL_DRIVER_VERSION/" dkms.conf.in > "/usr/src/limepcie-$KERNEL_DRIVER_VERSION/dkms.conf"
+# Default to using DKMS if available
+if command -v dkms --version &>/dev/null; then
+    MODE="dkms"
+else
+    MODE="legacy"
+fi
 
-dkms add -m limepcie/$KERNEL_DRIVER_VERSION
-dkms install -m limepcie/$KERNEL_DRIVER_VERSION
+# If first argument is an option
+if [[ $1 == -* ]]; then
+    if [[ $1 == "--dkms" && $MODE == "legacy" ]]; then
+        echo "DKMS mode was specified but DKMS is not installed; aborting"
+        exit 1
+    elif [[ $1 == "--legacy" ]]; then
+        MODE="legacy"
+    fi
+    shift
+fi
+
+if [[ $MODE == "legacy" ]]; then
+    MODULE_NAME=$1
+    if [ -z "$MODULE_NAME" ]; then
+        echo "Kernel module name not specified"
+        exit 1
+    fi
+
+    legacy_install
+else # DKMS mode
+    dkms_install
+fi
 
 exit 0
--- src/comms/PCIe/linux-kernel-module/uninstall.sh
+++ src/comms/PCIe/linux-kernel-module/uninstall.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
-KERNEL_DRIVER_VERSION="$(cat version)"
+
+function dkms_uninstall {
+    MODULE_AND_VERSION=$(dkms status | grep "limepcie" | cut -d ',' -f 1)
+    if [ "$MODULE_AND_VERSION" == "" ]; then
+        echo "Module not found; doing nothing."
+        exit 0
+    fi
+
+    printf "Uninstalling %s\n" "$MODULE_AND_VERSION"
+
+    dkms uninstall -m "$MODULE_AND_VERSION"
+    dkms remove -m "$MODULE_AND_VERSION"
+    rm -rf "/usr/src/$MODULE_AND_VERSION"
+
+    printf "Uninstalled %s\n" "$MODULE_AND_VERSION"
+}
+
+function legacy_uninstall {
+    if ! "$(dirname "$0")/unload.sh" "$MODULE_NAME"; then
+        exit 1
+    fi
+
+    # Disable auto loading of the module
+    echo "Removing module $MODULE_NAME from /etc/modules"
+    temp_file=$(mktemp)
+    chown --reference=/etc/modules "$temp_file"
+    chmod --reference=/etc/modules "$temp_file"
+    grep -vx "^$MODULE_NAME" /etc/modules >"$temp_file"
+    mv "$temp_file" /etc/modules
+}
 
 # Check root access
 if [ "$EUID" -ne 0 ]; then
@@ -7,8 +36,34 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-dkms uninstall -m limepcie/$KERNEL_DRIVER_VERSION
-dkms remove -m limepcie/$KERNEL_DRIVER_VERSION
-rm -rf "/usr/src/limepcie-$KERNEL_DRIVER_VERSION"
+# Default to using DKMS if available
+if command -v dkms --version &>/dev/null; then
+    MODE="dkms"
+else
+    MODE="legacy"
+fi
+
+# If first argument is an option
+if [[ $1 == -* ]]; then
+    if [[ $1 == "--dkms" && $MODE == "legacy" ]]; then
+        echo "DKMS mode was specified but DKMS is not installed; aborting"
+        exit 1
+    elif [[ $1 == "--legacy" ]]; then
+        MODE="legacy"
+    fi
+    shift
+fi
+
+if [[ $MODE == "legacy" ]]; then
+    MODULE_NAME=$1
+    if [ -z "$MODULE_NAME" ]; then
+        echo "Kernel module name not specified"
+        exit 1
+    fi
+
+    legacy_uninstall
+else
+    dkms_uninstall
+fi
 
 exit 0
