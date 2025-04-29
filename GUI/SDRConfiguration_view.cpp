9a7af037335b9e97a69bf69fe647c4acb545e751
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2025-04-29 12:47:34 +0300

    limeGUI: fix reference clock value in Configuration view

--- GUI/SDRConfiguration_view.cpp
+++ GUI/SDRConfiguration_view.cpp
@@ -257,7 +257,7 @@ void SOCConfig_view::SubmitConfig(const wxCommandEvent& event)
 
     SDRConfig config;
 
-    config.referenceClockFreq = 30.72e6;
+    config.referenceClockFreq = sdrDevice->GetClockFreq(0, 0);
 
     auto parseGuiValue = [](const wxString& str) {
         double value = 0;
