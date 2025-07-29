e57ed59393e73350c9d880afab1b89ac9562b485
Arnius <arnasg08@gmail.com>
2025-07-29 13:19:01 +0300

    Updated build script for gnuradio-limesuiteng conda package on Linux

--- plugins/gr-limesuiteng/.conda/recipe/build.sh
+++ plugins/gr-limesuiteng/.conda/recipe/build.sh
@@ -8,9 +8,12 @@ cd buildconda
 cmake_config_args=(
     -DCMAKE_BUILD_TYPE=Release
     -DCMAKE_INSTALL_PREFIX=$PREFIX
+    -DCMAKE_PREFIX_PATH=$PREFIX
+    -DGR_PYTHON_DIR=$SP_DIR
     -DLIB_SUFFIX=""
     -DENABLE_DOXYGEN=OFF
     -DENABLE_TESTING=ON
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.15
 )
 
 cmake ${CMAKE_ARGS} -G "Ninja" .. "${cmake_config_args[@]}"
