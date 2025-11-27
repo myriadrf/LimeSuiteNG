036d9c9d1e0e99e37672e9b8dff95c8db5b97fe7
Arnius <arnasg08@gmail.com>
2025-11-27 12:56:52 +0200

    Updated ICSR interface parameter comments.

--- src/comms/ICSR.h
+++ src/comms/ICSR.h
@@ -16,9 +16,9 @@ class LIME_API ICSR
     virtual ~ICSR() {}
     /**
       @brief Default path for writing/reading CSRegisters.
-      @param data_wr Buffer with data for writing CSRegisters.
+      @param data_wr Buffer with register address and register data blocks for writing CSRegisters.
       @param data_rd Buffer for storing read data from CSRegisters. Pass nullptr when wrinting only to CSRegisters.
-      @param count Number of CSRegisters to write/read.
+      @param count Number of CSRegisters to write/read. Not the size of data_wr buffer!
       @returns The operation status.
      */
     virtual OpStatus Transact(const uint64_t* data_wr, uint64_t* data_rd, uint32_t count) = 0;
