f925f389a4c410d2874f7d4713b9b65882a51190
Arnius <arnasg08@gmail.com>
2025-12-05 18:03:19 +0200

    Updated driver class declarations, member definitions and updated main function for limeGPSDO utility.

--- cli/limeGPSDO.cpp
+++ cli/limeGPSDO.cpp
@@ -7,6 +7,7 @@
 #include <cassert>
 #include <cstring>
 #include <filesystem>
+#include <array>
 #include "args.hxx"
 
 using namespace std;
@@ -17,46 +18,183 @@ using namespace lime::cli;
 class GPSDODriver
 {
    public:
-   GPSDODriver() CSR_interface(nullptr) : {}
-   GPSDODriver(ICSR * interface) CSR_interface(interface) : {}
-   ~GPSDODriver() { if (CSR_interface != nullptr) delete CSR_interface;}
 
-   uint64_t readRegister();
-   void writeRegister();
-   int32_t getSigned32bit();
-   int get_1s_error();
-   int get_10s_error();
-   int get_100s_error();
-   int getDacValue();
-   int getStatus();
-   int getEnabled();
-   int setEnabled();
+   const array<string,4> accuracyLevelList = {"Disabled/Lowest\n", "1s Tune\n", "2s Tune\n", "3s Tune (Highest)\n"}; 
+   static constexpr uint64_t reg_control            = 0x00000000F000B000;
+   static constexpr uint64_t reg_pps_1s_target      = 0x00000000F000B004;
+   static constexpr uint64_t reg_pps_1s_err_tol     = 0x00000000F000B008;
+   static constexpr uint64_t reg_pps_10s_target     = 0x00000000F000B00C;
+   static constexpr uint64_t reg_pps_10s_err_tol    = 0x00000000F000B010;
+   static constexpr uint64_t reg_pps_100s_target    = 0x00000000F000B014;
+   static constexpr uint64_t reg_pps_100s_err_tol   = 0x00000000F000B018;
+
+   static constexpr uint64_t reg_pps_1s_err         = 0x00000000F000B01C;
+   static constexpr uint64_t reg_pps_10s_err        = 0x00000000F000B020;
+   static constexpr uint64_t reg_pps_100s_err       = 0x00000000F000B024;
+   static constexpr uint64_t reg_dac_tuned_val      = 0x00000000F000B028;
+   static constexpr uint64_t reg_status_accuracy    = 0x00000000F000B02C;
+   static constexpr uint64_t reg_status_pps_active  = 0x00000000F000B030;
+   static constexpr uint64_t reg_status_state       = 0x00000000F000B034;
+
+   GPSDODriver() : mCSR_interface(nullptr) {}
+   GPSDODriver(ICSR * interface) : mCSR_interface(interface) {}
+   ~GPSDODriver() { if (mCSR_interface != nullptr) delete mCSR_interface;}
+
+   uint64_t readRegister(uint64_t address, OpStatus * status);
+   OpStatus writeRegister(uint64_t address, uint64_t value);
+   uint64_t getSigned32bit(uint64_t address, OpStatus * status);
+
+   uint64_t get_1s_error(OpStatus * status);
+   uint64_t get_10s_error(OpStatus * status);
+   uint64_t get_100s_error(OpStatus * status);
+   uint64_t getDacValue(OpStatus * status);
+   std::string getStatus();
+   uint64_t getEnabled(OpStatus * status);
+   uint64_t setEnabled(OpStatus * status);
 
    private:
-   ICSR * CSR_interface;
+   ICSR * mCSR_interface;
 };
 
-void runMonitoring()
+// ##################################################
+// #### GPSDODriver member functions definitions ####
+// ##################################################
+
+uint64_t GPSDODriver::readRegister(uint64_t address, OpStatus * status)
+{
+   return mCSR_interface->ioRead64(address, status);
+}
+
+OpStatus GPSDODriver::writeRegister(uint64_t address, uint64_t value)
+{
+   return mCSR_interface->ioWrite64(address, value);
+}
+
+uint64_t GPSDODriver::getSigned32bit(uint64_t address, OpStatus * status)
+{
+   uint64_t value = this->readRegister(address, status);
+   if(*status != OpStatus::Success)
+      return 0LL;
+   
+   if(value & (1 << 31))
+      value -= (1ULL << 32);
+
+   return value;
+}
+
+uint64_t GPSDODriver::get_1s_error(OpStatus * status)
+{
+   return this->getSigned32bit(GPSDODriver::reg_pps_1s_err, status);
+}
+
+uint64_t GPSDODriver::get_10s_error(OpStatus * status)
+{
+   return this->getSigned32bit(GPSDODriver::reg_pps_10s_err, status);
+}
+
+uint64_t GPSDODriver::get_100s_error(OpStatus * status)
+{
+   return this->getSigned32bit(GPSDODriver::reg_pps_100s_err, status)   ;
+}
+
+uint64_t GPSDODriver::getDacValue(OpStatus * status)
+{
+   return this->readRegister(GPSDODriver::reg_dac_tuned_val, status);
+}
+
+std::string GPSDODriver::getStatus()
+{
+   OpStatus status = OpStatus::Success;
+   std::string statusMsg;
+   uint64_t state = this->readRegister(GPSDODriver::reg_status_state, &status);
+   if(status != OpStatus::Success)
+   {
+      statusMsg += "Failed to read PPSDO_STATUS_STATE register with error:";
+      statusMsg += ToString(status);
+      statusMsg += "\n";
+      return statusMsg;
+   }
+
+   uint64_t accuracy = this->readRegister(GPSDODriver::reg_status_accuracy, &status);
+   if(status != OpStatus::Success)
+   {
+      statusMsg += "Failed to read PPSDO_STATUS_ACCURACY register with error:";
+      statusMsg += ToString(status);
+      statusMsg += "\n";
+      return statusMsg;
+   }
+
+   uint64_t tpulse = this->readRegister(GPSDODriver::reg_status_pps_active, &status);
+   if(status != OpStatus::Success)
+   {
+      statusMsg += "Failed to read PPSDO_STATUS_PPS_ACTIVE register with error:";
+      statusMsg += ToString(status);
+      statusMsg += "\n";
+      return statusMsg;
+   }
+
+   // TODO: Move this to formating function
+   statusMsg += "state: ";
+   if(state == 1ULL)
+      statusMsg += "Fine tune\n";
+   else if(state == 0ULL)
+      statusMsg += "Coarse tune\n";
+   else
+      statusMsg += "Unknown\n";
+   
+   statusMsg += "accuracy: ";
+   if(accuracy < 4)
+      statusMsg += GPSDODriver::accuracyLevelList[accuracy];
+   else
+   {
+      statusMsg += "Unknown(";
+      statusMsg += std::to_string(accuracy);
+      statusMsg +=")\n";
+   }
+
+   statusMsg += "tpulse_active: ";
+   statusMsg += tpulse ? "true\n" : "false\n";
+   
+   return statusMsg;   
+}
+
+uint64_t GPSDODriver::getEnabled(OpStatus * status)
+{
+   uint64_t value;
+   return value;
+}
+
+uint64_t GPSDODriver::setEnabled(OpStatus * status)
+{
+   uint64_t value;
+   return value;
+}
+
+// ###############################################
+// #### limeGPSDO helper function definitions ####
+// ###############################################
+
+void runMonitoring(GPSDODriver * pDriver)
 {
 
 }
 
-void dumpRegisters()
+void dumpRegisters(GPSDODriver * pDriver, int numberOfDumps = 1, int delay = 1)
 {
 
 }
 
-void resetGPSDO()
+void resetGPSDO(GPSDODriver * pDriver, int rstDelay = 2)
 {
 
 }
 
-void enableGPSDO()
+void enableGPSDO(GPSDODriver * pDriver, double clk = 30.72, double ppm = 0.1)
 {
 
 }
 
-void disableGPSDO()
+void disableGPSDO(GPSDODriver * pDriver)
 {
 
 }
@@ -76,6 +214,7 @@ int main(int argc, char** argv)
 
 
    args::Group                             arguments(parser, "arguments", args::Group::Validators::DontCare, args::Options::Global); // NOLINT(cppcoreguidelines-slicing)
+   args::ValueFlag<std::string>            deviceFlag(arguments, "name", "Specifies which device to use", {"device"}, "");
    args::ValueFlag<std::string>            num(arguments, "iter", "Number of iterations (for --check: 0 for infinite; for --dump: default 1 if not specified)", {'n', "num"}, "");
    args::ValueFlag<std::string>            delay(arguments, "time", "Delay between iterations (seconds, for --check and --dump)", {'d', "delay"}, "");
    args::ValueFlag<std::string>            banner(arguments, "interval", "Banner repeat interval (for --check)", {'b', "banner"}, "");
@@ -97,5 +236,50 @@ int main(int argc, char** argv)
       return EXIT_FAILURE;
    }
 
+   const std::string devName = args::get(deviceFlag);
 
+   auto handles = DeviceRegistry::enumerate();
+   if (handles.size() == 0)
+   {
+      cerr << "No devices found"sv << endl;
+      return EXIT_FAILURE;
+   }
+
+   SDRDevice* device = ConnectToFilteredOrDefaultDevice(devName);
+   if (!device)
+      return EXIT_FAILURE;
+
+   GPSDODriver driver(device->getICSR());
+
+   try
+   {
+      if(dump)
+         dumpRegisters(&driver);
+      else if(enable)
+         enableGPSDO(&driver);
+      else if(disable)
+         disableGPSDO(&driver);
+      else if(reset)
+         resetGPSDO(&driver);
+      else if(check)
+         runMonitoring(&driver);
+      else
+      {
+         cerr << "No command specified! Aborting limeGPSDO execution!";
+         driver.~GPSDODriver();
+         DeviceRegistry::freeDevice(device);
+         return EXIT_FAILURE;
+      }
+
+   }
+   catch(const std::exception& e)
+   {
+      driver.~GPSDODriver();
+      DeviceRegistry::freeDevice(device);
+      std::cerr << e.what() << '\n';
+   }
+      
+   driver.~GPSDODriver();
+   DeviceRegistry::freeDevice(device);
+   return EXIT_SUCCESS;
 }
