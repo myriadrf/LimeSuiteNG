03f86903f37aac41bc8152acb4c3c6f22be8e3a8
Arnius <arnasg08@gmail.com>
2025-12-23 11:23:18 +0200

    Formated limeCSR according to .clang-format file options.

--- cli/limeCSR.cpp
+++ cli/limeCSR.cpp
@@ -14,101 +14,100 @@ using namespace std;
 using namespace lime;
 using namespace lime::cli;
 
-
 static void PrintCSRegReadResults(std::ostream& stream, const std::vector<uint64_t>& rd_data, const std::vector<uint64_t>& wr_data)
-{ 
-   stream << "CSR data read out (address | value):" << endl;
-   stream << std::hex << std::setfill('0');
-   for(uint64_t i = 0; i < wr_data.size(); ++i)
-      stream << "0x" << std::setw(16) << wr_data[i] << " 0x" << std::setw(16) << rd_data[i] << std::endl;
+{
+    stream << "CSR data read out (address | value):" << endl;
+    stream << std::hex << std::setfill('0');
+    for (uint64_t i = 0; i < wr_data.size(); ++i)
+        stream << "0x" << std::setw(16) << wr_data[i] << " 0x" << std::setw(16) << rd_data[i] << std::endl;
 }
 
 static uint64_t hex2ULLint(const std::string_view hexstr)
 {
-   uint64_t value = 0;
-   sscanf(hexstr.data(), "%016" SCNx64 "", &value);
-   return value;
+    uint64_t value = 0;
+    sscanf(hexstr.data(), "%016" SCNx64 "", &value);
+    return value;
 }
 
 static int parseWriteInput(std::string_view hexstr, std::vector<uint64_t>& wr_data)
 {
-   static const std::string_view delimiters = " \n,"sv;
-   wr_data.clear();
-   int tokenCount = 0;
+    static const std::string_view delimiters = " \n,"sv;
+    wr_data.clear();
+    int tokenCount = 0;
 
-   std::size_t position = 0;
-   while (position != std::string_view::npos)
-   {
-      position = hexstr.find_first_of(delimiters);
-      std::string_view token = hexstr.substr(0, position);
-      int tokenLength = token.size();
-      if (tokenLength <= 32 && tokenLength > 16) // write instruction
-      {
-         uint64_t addr = hex2ULLint(token.substr(0,16));
-         uint64_t value = hex2ULLint(token.substr(16,32));
-         wr_data.push_back(addr);
-         wr_data.push_back(value);
-      }
-      else if (tokenLength != 0)
-      {
-         std::cerr << "ERROR: Invalid input value: "sv << token << std::endl;
-      }
-      ++tokenCount;
-      hexstr = hexstr.substr(position + 1);
-   }
-   return tokenCount;
+    std::size_t position = 0;
+    while (position != std::string_view::npos)
+    {
+        position = hexstr.find_first_of(delimiters);
+        std::string_view token = hexstr.substr(0, position);
+        int tokenLength = token.size();
+        if (tokenLength <= 32 && tokenLength > 16) // write instruction
+        {
+            uint64_t addr = hex2ULLint(token.substr(0, 16));
+            uint64_t value = hex2ULLint(token.substr(16, 32));
+            wr_data.push_back(addr);
+            wr_data.push_back(value);
+        }
+        else if (tokenLength != 0)
+        {
+            std::cerr << "ERROR: Invalid input value: "sv << token << std::endl;
+        }
+        ++tokenCount;
+        hexstr = hexstr.substr(position + 1);
+    }
+    return tokenCount;
 }
 
 static int parseReadInput(std::string_view hexstr, std::vector<uint64_t>& wr_data)
 {
-   static const std::string_view delimiters = " \n,"sv;
-   wr_data.clear();
-   int tokenCount = 0;
+    static const std::string_view delimiters = " \n,"sv;
+    wr_data.clear();
+    int tokenCount = 0;
 
-   std::size_t position = 0;
-   while (position != std::string_view::npos)
-   {
-      position = hexstr.find_first_of(delimiters);
-      std::string_view token = hexstr.substr(0, position);
-      int tokenLength = token.size();
-      if (tokenLength <= 16 && tokenLength > 0) // read instruction
-      {
-         uint64_t addr = hex2ULLint(token);
-         wr_data.push_back(addr);
-      }
-      else if (tokenLength != 0)
-      {
-         std::cerr << "ERROR: Invalid input value: "sv << token << std::endl;
-      }
-      ++tokenCount;
-      hexstr = hexstr.substr(position + 1);
-   }
-   return tokenCount;
+    std::size_t position = 0;
+    while (position != std::string_view::npos)
+    {
+        position = hexstr.find_first_of(delimiters);
+        std::string_view token = hexstr.substr(0, position);
+        int tokenLength = token.size();
+        if (tokenLength <= 16 && tokenLength > 0) // read instruction
+        {
+            uint64_t addr = hex2ULLint(token);
+            wr_data.push_back(addr);
+        }
+        else if (tokenLength != 0)
+        {
+            std::cerr << "ERROR: Invalid input value: "sv << token << std::endl;
+        }
+        ++tokenCount;
+        hexstr = hexstr.substr(position + 1);
+    }
+    return tokenCount;
 }
 
 static std::string ReadFile(const std::string& fileName)
 {
-   std::vector<char> buffer;
-   std::ifstream inputFile(fileName);
-   if (!inputFile.is_open())
-   {
-      cerr << "ERROR: Failed to open file: "sv << fileName << endl;
-      exit(EXIT_FAILURE);
-   }
-   inputFile.seekg(0, std::ios::end);
-   long fileSize = inputFile.tellg();
-   inputFile.seekg(0, std::ios::beg);
+    std::vector<char> buffer;
+    std::ifstream inputFile(fileName);
+    if (!inputFile.is_open())
+    {
+        cerr << "ERROR: Failed to open file: "sv << fileName << endl;
+        exit(EXIT_FAILURE);
+    }
+    inputFile.seekg(0, std::ios::end);
+    long fileSize = inputFile.tellg();
+    inputFile.seekg(0, std::ios::beg);
 
-   buffer.resize(fileSize);
-   inputFile.read(&buffer[0], fileSize);
-   inputFile.close();
-   buffer[fileSize] = 0;
-   return buffer.data();
+    buffer.resize(fileSize);
+    inputFile.read(&buffer[0], fileSize);
+    inputFile.close();
+    buffer[fileSize] = 0;
+    return buffer.data();
 }
 
 int main(int argc, char** argv)
 {
-   // clang-format off
+    // clang-format off
    args::ArgumentParser                    parser("limeCSR - Configuration Space Registers I/O", "");
    args::HelpFlag                          help(parser, "help", "This help", {'h', "help"});
 
@@ -128,105 +127,106 @@ int main(int argc, char** argv)
                                                                            " command example:\n --stream=100cc000007000801a0000e00000feed,"
                                                                            "0000000000700080000000e00000feed\n  Read command example:\n "
                                                                            "--stream=100cc00000700080,0000000000700080\n", {'s', "stream"});
-   // clang-format on
+    // clang-format on
 
-   try
-   {
-      parser.ParseCLI(argc, argv);
-   } catch (args::Help&)
-   {
-      cout << parser << endl;
-      return EXIT_SUCCESS;
-   } catch (const std::exception& e)
-   {
-      cerr << "ERROR: " << e.what() << std::endl;
-      return EXIT_FAILURE;
-   }
+    try
+    {
+        parser.ParseCLI(argc, argv);
+    } catch (args::Help&)
+    {
+        cout << parser << endl;
+        return EXIT_SUCCESS;
+    } catch (const std::exception& e)
+    {
+        cerr << "ERROR: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-   if (fileFlag == streamFlag)
-   {
-      cerr << "ERROR: Either -s or -f must be provided ONCE" << endl;
-      return EXIT_FAILURE;
-   }
+    if (fileFlag == streamFlag)
+    {
+        cerr << "ERROR: Either -s or -f must be provided ONCE" << endl;
+        return EXIT_FAILURE;
+    }
 
-   const std::string devName = args::get(deviceFlag);
-   const std::string hexInput = streamFlag ? args::get(streamFlag) : ReadFile(args::get(fileFlag));
+    const std::string devName = args::get(deviceFlag);
+    const std::string hexInput = streamFlag ? args::get(streamFlag) : ReadFile(args::get(fileFlag));
 
-   if (hexInput.empty())
-   {
-      cerr << "ERROR: No input provided"sv << endl;
-      return EXIT_FAILURE;
-   }
+    if (hexInput.empty())
+    {
+        cerr << "ERROR: No input provided"sv << endl;
+        return EXIT_FAILURE;
+    }
 
-   auto handles = DeviceRegistry::enumerate();
-   if (handles.size() == 0)
-   {
-      cerr << "ERROR: No devices found"sv << endl;
-      return EXIT_FAILURE;
-   }
+    auto handles = DeviceRegistry::enumerate();
+    if (handles.size() == 0)
+    {
+        cerr << "ERROR: No devices found"sv << endl;
+        return EXIT_FAILURE;
+    }
 
-   SDRDevice* device = ConnectToFilteredOrDefaultDevice(devName);
-   if (!device)
-   {
-      cerr << "ERROR: Failed to connect to SDR device!\n";
-      return EXIT_FAILURE;
-   }
+    SDRDevice* device = ConnectToFilteredOrDefaultDevice(devName);
+    if (!device)
+    {
+        cerr << "ERROR: Failed to connect to SDR device!\n";
+        return EXIT_FAILURE;
+    }
 
-   ICSR * CSR_interface = device->getICSR();
-   if(CSR_interface == nullptr)
-   {
-      cerr << "ERROR: Selected SDR device does not support CSR interface!\n";
-      DeviceRegistry::freeDevice(device);
-      return EXIT_FAILURE;
-   }
+    ICSR* CSR_interface = device->getICSR();
+    if (CSR_interface == nullptr)
+    {
+        cerr << "ERROR: Selected SDR device does not support CSR interface!\n";
+        DeviceRegistry::freeDevice(device);
+        return EXIT_FAILURE;
+    }
 
-   std::vector<uint64_t> wr_data;
-   std::vector<uint64_t> rd_data;
-   
-   if(write)
-      parseWriteInput(hexInput, wr_data);
-   else if(read)
-   {
-      parseReadInput(hexInput, wr_data);
-      rd_data.resize(wr_data.size());
-   }
+    std::vector<uint64_t> wr_data;
+    std::vector<uint64_t> rd_data;
 
-   try
-   {   
-      cerr << setfill('0');
-      if(write)
-      {
-         for(uint64_t i = 0; i < wr_data.size(); i+=2)
-         {
-            OpStatus status = CSR_interface->ioWrite64(wr_data[i], wr_data[i+1]);
-            if(status != OpStatus::Success)
-               cerr << "ERROR: CSR write failed for register address 0x" << hex << setw(16) << wr_data[i] << dec << " with error: " << ToString(status) << endl;
-         }
-      }
-      else if(read)
-      {
-         OpStatus status = OpStatus::Success;
-         for(uint64_t i = 0; i < wr_data.size(); ++i)
-         {
-            rd_data[i] = CSR_interface->ioRead64(wr_data[i], &status);
-            if(status != OpStatus::Success)
-               cerr << "ERROR: CSR read failed for register address 0x" << hex << setw(16) << wr_data[i] << dec << " with error: " << ToString(status) << endl;
-         }
-      }
-      
+    if (write)
+        parseWriteInput(hexInput, wr_data);
+    else if (read)
+    {
+        parseReadInput(hexInput, wr_data);
+        rd_data.resize(wr_data.size());
+    }
 
-   } catch (std::runtime_error& e)
-   {
-      delete CSR_interface;
-      DeviceRegistry::freeDevice(device);
-      cerr << "ERROR: CSR operation failed: "sv << e.what() << endl;
-      return EXIT_FAILURE;
-   }
+    try
+    {
+        cerr << setfill('0');
+        if (write)
+        {
+            for (uint64_t i = 0; i < wr_data.size(); i += 2)
+            {
+                OpStatus status = CSR_interface->ioWrite64(wr_data[i], wr_data[i + 1]);
+                if (status != OpStatus::Success)
+                    cerr << "ERROR: CSR write failed for register address 0x" << hex << setw(16) << wr_data[i] << dec
+                         << " with error: " << ToString(status) << endl;
+            }
+        }
+        else if (read)
+        {
+            OpStatus status = OpStatus::Success;
+            for (uint64_t i = 0; i < wr_data.size(); ++i)
+            {
+                rd_data[i] = CSR_interface->ioRead64(wr_data[i], &status);
+                if (status != OpStatus::Success)
+                    cerr << "ERROR: CSR read failed for register address 0x" << hex << setw(16) << wr_data[i] << dec
+                         << " with error: " << ToString(status) << endl;
+            }
+        }
 
-   if (read)
-      PrintCSRegReadResults(cout, rd_data, wr_data);
+    } catch (std::runtime_error& e)
+    {
+        delete CSR_interface;
+        DeviceRegistry::freeDevice(device);
+        cerr << "ERROR: CSR operation failed: "sv << e.what() << endl;
+        return EXIT_FAILURE;
+    }
 
-   delete CSR_interface;
-   DeviceRegistry::freeDevice(device);
-   return EXIT_SUCCESS;
+    if (read)
+        PrintCSRegReadResults(cout, rd_data, wr_data);
+
+    delete CSR_interface;
+    DeviceRegistry::freeDevice(device);
+    return EXIT_SUCCESS;
 }
