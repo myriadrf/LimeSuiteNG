6f0cbedfc84dad570444e37dd45bb0d149e09e2b
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2026-02-03 10:53:29 +0200

    limesdr-micro: fix sampling rate readback.
    Read ADC/DAC clock dividers directly from hardware registers, not from host interface registers

--- src/chips/LA9310/LA9310.cpp
+++ src/chips/LA9310/LA9310.cpp
@@ -31,10 +31,25 @@ OpStatus LA9310::SetSystemClock(double sysClk_Hz, uint8_t adc_rate_mask, uint8_t
 
 void LA9310::GetADCDACRates(uint8_t* adc_rate_mask, uint8_t* dac_rate_mask)
 {
+    constexpr uint32_t DCS_BASE_ADDR_offset = 0x1040000;
+    volatile uint8_t* DCS = reinterpret_cast<uint8_t*>(pcie->GetBar(LA9310_WINDOW_BAR0).vaddr) + DCS_BASE_ADDR_offset;
+
+    volatile uint32_t* ADC_DAC_CLKCFG = reinterpret_cast<volatile uint32_t*>(DCS + 0x300);
+    volatile uint32_t* ADC_DAC_CLKCTRL = reinterpret_cast<volatile uint32_t*>(DCS + 0x310);
+
+    uint32_t dividers = *ADC_DAC_CLKCFG;
+
+    const uint32_t dacDividerBy2 = (dividers >> 16) & 1;
+    const uint32_t adcDividerBy2 = ((dividers >> 6) & 0xC) | dividers & 0x3;
+
+    const uint32_t dividersEnabled = *ADC_DAC_CLKCTRL;
+    const uint32_t rxDividersEn = dividersEnabled & 0xF;
+    const uint32_t txDividerEn = (dividersEnabled >> 16) & 0x1;
+
     if (adc_rate_mask)
-        *adc_rate_mask = hif->adc_rate_mask;
+        *adc_rate_mask = rxDividersEn & adcDividerBy2;
     if (dac_rate_mask)
-        *adc_rate_mask = hif->adc_rate_mask;
+        *dac_rate_mask = txDividerEn & dacDividerBy2;
 }
 
 bool LA9310::IsM4CoreProgrammed()
