f0a165a61a9e4d9485ceb98cb67e6d5aa5f7f277
Arnius <arnasg08@gmail.com>
2026-03-31 16:39:41 +0300

    Added delete mode for doxygen converter script.

--- docs/dox_converter.py
+++ docs/dox_converter.py
@@ -2,31 +2,39 @@
 Doxygen to Sphinx page converter
 ================================
 
-Python module defines additional functions that can be used to quickly auto parse the regular doxygen pages and generate
-the equivalent sphinx page files (.rst files) with the required Sphinx Breathe plugin directives. 
+Python module performs quick auto parsing of the regular doxygen pages (with @page tag) and generates 
+the equivalent sphinx page files (.rst files) with the required Sphinx Breathe plugin directives. This 
+module can be run in reverse mode to delete all the generated manual pages (.rst files). See supported 
+flags for more info.
 
 Usage:
-   ``python dox_converter.py [override-root-path]``
+   ``python dox_converter.py [override-root-path] [optional-flags]``
 
 This module will auto scan each subdirectory starting from the root directory (by default that is ``./doxygen``) and will
-generate .rst files equivalent to all the .dox files. To override the root directory, pass the root directory as a command line 
-argument when calling the module.
+generate .rst files equivalent to all the .dox files. To override the root directory, pass the root directory as a command 
+line argument when calling the module.
+
+Supported flags:
+
+* `--del` - Runs dox_converter in reverse. Deletes all the .rst manual pages that were converted from doxygen manual pages.
 
 This module does not support the auto generation of index files used to organize navigation between pages.
 Users are responsible for setting up the correct index tree. Module will ignore files that have the following 
 names: ``mainpage.dox, index.dox``. Therefore, it is recommended to use these file names for doxygen index pages. 
-Else the module can produce undesired results.
+If index page naming is ignored, the module can produce undesired results.
 
 """
 
 from pathlib import Path
 import sys
 
+reverse = False                  # Dox converter in reverse mode: deletes all of the generated manual pages in .rst format.
 root_path = Path("./doxygen/")   # Default root path
-Gfile_suffix = ".dox"            # Global for file suffix search
+dox_suffix = ".dox"              # Doxygen suffix indentifier
+rst_suffix = ".rst"              # Sphinx file suffix indentifier
 dir_tree = []                    # The directory tree view from the root path 
 
-index_indicators = ["mainpage.dox", "index.dox"]        # File names that indicate index file with toc tree
+index_indicators = ["mainpage", "index"]        # File names that indicate index file with toc tree
 ignore_dirs = ["output", "api_member_list", "images"]   # Directories to ignore
 
 
@@ -49,12 +57,12 @@ def update_dir_tree(start_dir, ignore_list = ignore_dirs):
       else:
          continue
 
-def get_files(start_dir):
+def get_files(start_dir, suffix):
    """Gets the list of all files in specified diretory."""
 
    files = []
    for dir_item in start_dir.iterdir():
-      if dir_item.is_file() and dir_item.suffix == Gfile_suffix:
+      if dir_item.is_file() and dir_item.suffix == suffix:
          files.append(dir_item)
       else:
          continue
@@ -76,11 +84,18 @@ def convert_page(file):
    rst.close()
 
 def convert_pages(files):
-   """Generates sphinx compatible rst pages with breathe directives from doxygen pages."""
+   """Generates sphinx compatible rst manual pages with breathe directives from doxygen pages."""
 
    for file in files:
-      if file.name not in index_indicators:
+      if file.stem not in index_indicators:
          convert_page(file)
+
+def remove_pages(files):
+   """Removes sphinx compatible rst manual pages."""
+
+   for file in files:
+      if file.stem not in index_indicators:
+         file.unlink()
 
 def print_tree():
    """Prints directory tree with contents of each directory."""
@@ -97,15 +112,26 @@ def print_tree():
 ########################################
 
 if len(sys.argv) > 1:
-   root_path = Path(f"{sys.argv[1]}")
-   print(f"Doxygen page converter: Switching to new root path - {sys.argv[1]}")
+   for arg in sys.argv[1:]:
+      if arg == "--del":
+         reverse = True
+      else:
+         root_path = Path(f"{arg}")
+         print(f"Doxygen page converter: Switching to new root path - {arg}")
 
 update_dir_tree(root_path)
 # print_tree()
 
+# Delete generated rst manual pages
+if reverse:
+   for dir in dir_tree:
+      files = get_files(dir, rst_suffix)
+      remove_pages(files)
+
 # Generate regular rst pages from regular doxygen pages
-for dir in dir_tree:
-   files = get_files(dir)
-   convert_pages(files)
+else:
+   for dir in dir_tree:
+      files = get_files(dir, dox_suffix)
+      convert_pages(files)
    
 # print_tree()
